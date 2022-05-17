763af990cfcf5d656a96f040aaa7b4a325819733
Maxime Badin <maxbadin38@gmail.com>
2022-05-17 15:01:45 +0200

    Step n°2

--- App.js
+++ App.js
@@ -1,31 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {FlatList, StyleSheet, View} from 'react-native';
 import {useEffect, useState} from "react";
 import TheCocktailDBApiService from "./src/service/TheCocktailDBApiService";
+import CocktailListItem from "./src/component/CocktailListItem";
+import {Text} from "react-native";
 
 export default function App() {
 
-  const [cocktailList, setCocktailList] = useState([])
+    const [cocktailList, setCocktailList] = useState([])
 
-  useEffect(() => {
-    TheCocktailDBApiService.getMultipleItems(5).then((array) => {
-      setCocktailList(array);
-    })
-  }, [])
+    useEffect(() => {
+        TheCocktailDBApiService.getMultipleItems(20).then((array) => {
+            setCocktailList(array);
+        })
+    }, [])
 
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const loadMoreCocktail = async () => {
+        const cocktails = await TheCocktailDBApiService.getMultipleItems(6)
+        setCocktailList((m) => {
+            return m.concat(cocktails);
+        });
+    };
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.textHeader}>Cocktails</Text>
+            <FlatList numColumns={2} data={cocktailList}
+                      keyExtractor={(item, index) => item.idDrink }
+                      onEndReached={loadMoreCocktail}
+                      renderItem={({item, index}) => {
+                return (<CocktailListItem item={item} key={index}/>)
+            }}>
+            </FlatList>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#2b2432',
+        color: "#fff",
+        paddingVertical: 40,
+        paddingHorizontal: 20
+    },
+    textHeader: {
+        textAlign: "left",
+        fontSize: 24,
+        color: "#FFF"
+    }
 });
--- src/component/CocktailListItem.jsx
+++ src/component/CocktailListItem.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {StyleSheet, Text, View, Image} from 'react-native';
 
 const CocktailListItem = ({item}) => {
     return (
-        <Text>{item.strDrink}</Text>
+        <View style={styles.container}>
+            <Image source={{uri: item.strDrinkThumb}} style={styles.picture}/>
+            <Text style={styles.name}>{item.strDrink}</Text>
+        </View>
     );
 }
 
 export default CocktailListItem;
+const styles = StyleSheet.create({
+    container: {
+        flex: 1/2,
+        backgroundColor: "#1c1426",
+        margin: 5,
+        borderRadius: 20,
+
+    },
+    name: {
+        margin: 10,
+        textAlign: "center",
+        color: "#fff"
+    },
+    picture: {
+        height: 150,
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
+    }
+});
